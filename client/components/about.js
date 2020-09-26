3848f1dcbb3742a41c4555f572b49165f9a15be2
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2020-09-26 06:58:11 +0000

    :memo: Update README

--- client/components/about.js
+++ client/components/about.js
@@ -1,1 +1,27 @@
-import React from '
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+
+const About = () => {
+
+  const styles = {
+    container: {
+      paddingLeft: 200,
+      paddingRight: 200,
+      paddingTop: 50
+    },
+    header: {
+      marginBottom: 40
+    },
+    content: {
+      lineheight: 2
+    }
+  }
+
+  return (
+    <div style={styles.container}>
+
+      <Typography
+        variant="display1"
+        align="center"
+        style={styles.header}
