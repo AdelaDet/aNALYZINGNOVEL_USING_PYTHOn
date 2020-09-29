07ac2af99b3eb27a8c2e2136b81004ae8562269f
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2020-09-29 22:41:51 +0000

    Off by one

--- client/components/category/category-all-paths.js
+++ client/components/category/category-all-paths.js
@@ -1,2 +1,27 @@
 import React from 'react'
-import 
+import  Grid from '@material-ui/core/Grid'
+import {PathCard} from '../paths'
+
+const styles = {
+  container: {
+    padding: '0 20px',
+    textAlign: 'center',
+    marginTop: 50
+  },
+  header: {
+    textAlign: 'center',
+    fontWeight: 300,
+    fontSize: 28
+  },
+  subheader: {
+    textAlign: 'center',
+    paddingBottom: 40
+  }
+}
+
+const CategoryAllPaths = (props) => {
+  const {paths, categoryName} = props
+  const pathsCount = paths.length
+  return(
+    <div style={styles.container}>
+      <h2 style={styles.header}>{
