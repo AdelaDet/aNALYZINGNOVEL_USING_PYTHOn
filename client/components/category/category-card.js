5eedb54b61193b566f5cc4ab90d1ac5f6cbdb9f4
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2020-10-17 10:39:02 +0000

    Update VSCode settings

--- client/components/category/category-card.js
+++ client/components/category/category-card.js
@@ -1,4 +1,24 @@
 import React from 'react'
 import Card from '@material-ui/core/Card'
 import { Link } from 'react-router-dom'
-import Button f
+import Button from '@material-ui/core/Button'
+
+const styles = {
+  container: {
+    paddingBottom: 30,
+    paddingTop: 20
+  },
+  header: {
+    fontSize: 32,
+    fontWeight: 300
+  }
+}
+
+const CategoryCard = ({imageSlug, categoryName, pathCount}) => {
+  return(
+    <div>
+      <Link to={`/category/${categoryName}`}>
+        <Card style={styles.container}>
+          <img src={`/category-logos/${imageSlug}.png`} width={75} />
+          <h2 style={styles.header}>{categoryName}</h2>
+          <Button color="prim
