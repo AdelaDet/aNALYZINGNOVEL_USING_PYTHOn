74a1b007f1a75cd567e076fb2d04780a64d70d34
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2020-10-18 14:31:49 +0000

    footer changes

--- client/components/category/category-dropdown.js
+++ client/components/category/category-dropdown.js
@@ -6,4 +6,30 @@ import { getAllParentCategoriesThunk } from '../../store'
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import
+import Fade from '@material-ui/core/Fade';
+
+class CategoryDropdown extends Component {
+  state = {
+    anchorEl: null,
+  }
+
+  componentDidMount = () => {
+    this.props.getParentCategories()
+  }
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render(){
+    const { anchorEl } = this.state
+    const {parentCategories} = this.props
+
+    return (
+      <div>
+        <Button
+      
