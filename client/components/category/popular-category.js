c175474354893c41a347433f8490cfe0444b8564
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2020-11-23 05:30:55 +0000

    fix css path

--- client/components/category/popular-category.js
+++ client/components/category/popular-category.js
@@ -7,4 +7,27 @@ import Grid from '@material-ui/core/Grid'
 const styles = {
   container: {
     padding: '0 20px',
- 
+    textAlign: 'center',
+    marginBottom: 50,
+  },
+  inner: {
+    textAlign: 'center',
+    centerWrap: {
+      display: 'inline-block'
+    }
+  },
+  header: {
+    textAlign: 'center',
+    fontWeight: 300,
+    paddingBottom: 40,
+    fontSize: 36
+  }
+}
+
+class PopularCategories extends React.Component {
+  componentDidMount(){
+    this.props.getPopularCategories()
+  }
+
+  createImageSlug = (str) => {
+    return str.toLowerCase().repla
