dbdb912df1596a5158b8c0bc4b2d49a7dcab54c2
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2020-12-11 22:28:39 +0000

    Removed unused var

--- client/components/footer.js
+++ client/components/footer.js
@@ -7,4 +7,30 @@ const styles = {
     height: 110,
     borderTop: '1px solid #efefef',
     backgroundColor: 'white',
+    marginTop: 100,
+    textAlign: 'center',
+    paddingTop: 10
+  },
+  content: {
+    height: 100,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    resize: 'vertical',
+    overflow: 'auto'
+  },
+  links: {
+    display: 'inline-block',
+    paddingBottom: 10
+  },
+  logo: {
+    margin: '10px 0',
+    fontSize: 18
+  }
+}
+
+const Footer = () => {
+  return (
+    <footer style={styles.footer}>
+      <div style={styles.content}>
  
