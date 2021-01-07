a2008056efd80c29e0b775475ec0c62efa80e757
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2021-01-07 17:37:00 +0000

    change LICENSE

--- client/components/paths/add-resource-details.js
+++ client/components/paths/add-resource-details.js
@@ -1,3 +1,28 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { addStepToPathThunk, getSinglePathB
+import { addStepToPathThunk, getSinglePathByUidThunk } from '../../store'
+
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator'
+import Button from '@material-ui/core/Button'
+
+const styles = {
+  button: {
+    marginTop: 30
+  }
+}
+
+class AddResourceDetails extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      title: '',
+      description: '',
+      imageUrl: '',
+      type1: ''
+    }
+  }
+
+  componentWillMount = () => {
+    this.setState({
+      title: this.props.resource[0].name
