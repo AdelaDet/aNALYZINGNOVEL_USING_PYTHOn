9d9e21282e277f0bf508bfc79fc0a83a77988256
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2021-02-16 21:49:48 +0000

    Javadoc cleanup

--- client/components/paths/builder-level.js
+++ client/components/paths/builder-level.js
@@ -4,4 +4,24 @@ import { withStyles } from '@material-ui/core/styles'
 import FormControl from '@material-ui/core/FormControl'
 import FormLabel from '@material-ui/core/FormLabel'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Radio from '@material-u
+import Radio from '@material-ui/core/Radio'
+import RadioGroup from '@material-ui/core/RadioGroup'
+
+const styles = theme => ({
+  formControl: {
+    display: 'block',
+    width: '100%',
+    marginTop: 40
+  },
+  group: {
+    display: 'inline'
+  },
+});
+
+const BuilderLevel = ({classes, level}) => {
+  return (
+    <FormControl component="fieldset" required className={classes.formControl}>
+      <FormLabel component="legend">What skill level is your path?</FormLabel>
+      <RadioGroup
+        aria-label="level"
+        name=
