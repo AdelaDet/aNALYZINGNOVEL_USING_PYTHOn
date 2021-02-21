54f3c31f8928a183dd1310792fb89620a56bdd3e
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2021-02-21 16:39:31 +0000

    merge with github

--- client/components/paths/path-card-category.js
+++ client/components/paths/path-card-category.js
@@ -10,4 +10,25 @@ width: 400px;
 height: 150px;
 box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
 display: flex;
-padding:
+padding: 0;
+overflow:auto;
+background-color: white;
+justify-content: flex-start
+`
+
+const CardContent = styled.div`
+  padding: 10px;
+  box-sizing: border-box;
+  font-weight: 200;
+  line-height: normal;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  margin-top: 10px;
+`
+
+const PathCardSmallCategory = (props) => {
+   const {img, name, rating, description, uid, slug} = props
+   const url = props.url || 'home'
+    return(
+  
