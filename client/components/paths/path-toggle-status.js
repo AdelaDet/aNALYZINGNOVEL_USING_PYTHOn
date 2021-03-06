009726daa9521441770a9751011f74e02534d8f0
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2021-03-06 15:48:52 +0000

    fs: Fix possible use-after-free with AIO

--- client/components/paths/path-toggle-status.js
+++ client/components/paths/path-toggle-status.js
@@ -1,4 +1,25 @@
 import React, {Component} from 'react'
 import Switch from '@material-ui/core/Switch'
 
-class PathTog
+class PathToggleStatus extends Component {
+
+  handleChange = () => {
+    this.props.toggle(this.props.uid, this.props.Status === 'draft' ? 'public' : 'draft', this.props.username)
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Toggle Path Public or Private</p>
+        <Switch
+          checked={this.props.Status === 'public'}
+          onChange={this.handleChange}
+          value="status"
+          color="primary"
+        />
+      </div>
+    )
+  }
+}
+
+export default PathToggleStatus
