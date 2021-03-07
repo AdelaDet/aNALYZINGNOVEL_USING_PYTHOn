34a68c120869932bed9197939d2b08e9e24eb08d
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2021-03-07 05:41:31 +0000

    Edit gitignore

--- client/components/paths/path-user-directory.js
+++ client/components/paths/path-user-directory.js
@@ -1,1 +1,23 @@
-imp
+import React from 'react'
+import { Link } from 'react-router-dom'
+import MenuList from '@material-ui/core/MenuList'
+import MenuItem from '@material-ui/core/MenuItem'
+
+const styles = {
+  container: {
+    backgroundColor: 'white',
+    borderWidth: '1px',
+    borderColor: '#efefef',
+    borderStyle: 'solid'
+  },
+  header: {
+    textAlign: 'center'
+  },
+  noPaths: {
+    textAlign: 'center',
+    paddingLeft: 20,
+    paddingRight: 20
+  }
+}
+
+const PathUser
