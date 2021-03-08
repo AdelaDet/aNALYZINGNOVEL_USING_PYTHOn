6fd84f17d22d90512740e08942d24e2df03757ed
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2021-03-08 17:38:54 +0000

    Added build status to README.md

--- client/components/reviews/review-card.js
+++ client/components/reviews/review-card.js
@@ -1,2 +1,25 @@
 import React from 'react'
-import List from '@material-ui/cor
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import {Stars} from '../reviews'
+
+const styles = {
+  container: {
+    backgroundColor: 'white',
+    borderWidth: '1px',
+    borderColor: '#efefef',
+    borderStyle: 'solid'
+  }
+}
+
+const ReviewCard = (props) => {
+
+  return(
+    <div style={styles.container}>
+    <List>
+      <ListItem>
+      <ListItemText>
+        {props.author}
+      </ListItemText>
+      <Sta
