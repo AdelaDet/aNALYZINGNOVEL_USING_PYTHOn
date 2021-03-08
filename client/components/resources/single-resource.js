7f312d9ae119cffed52193067acc6e38bcbc7e88
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2021-03-08 14:15:11 +0000

    Fixing an important issue with the universe.

--- client/components/resources/single-resource.js
+++ client/components/resources/single-resource.js
@@ -1,3 +1,19 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import styled f
+import styled from "styled-components"
+
+
+
+
+const Resource = (props) => {
+  const {url, name} = props
+  return(
+    <div>
+      <p>this link takes you to an outside resource</p>
+      <a href={url}>{name}</a>
+      <p>happy learning!</p>
+      </div>
+  )
+}
+
+export default (Resource)
---
+++ client/components/reviews/index.js
@@ -0,0 +1,3 @@
+export {default as ReviewCard} from './review-card.js'
+export {default as Stars} from './stars.js'
+export {default as ReviewPathDialog} from './review-path-dialog'
