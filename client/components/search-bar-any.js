f11736fbfa4a69baaa116b82595598e03a8bd3a9
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2021-03-09 07:41:06 +0000

    ci(flux): [skip ci] Update image from automation

--- client/components/search-bar-any.js
+++ client/components/search-bar-any.js
@@ -1,4 +1,30 @@
 import React, {Component} from 'react'
 import { withRouter, Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-import styled from "styled-componen
+import styled from "styled-components"
+
+import { createFuzzyMatchThunk, createMatchAllInCategoryThunk } from '../store'
+
+const DropDown = styled.div`
+  position: absolute;
+  z-index: 0;
+  background-color: white;
+  padding-left: 40px;
+  max-height:200px;
+  overflow: scroll;
+  width: 600px;
+`
+
+const MatchRow = styled.div`
+  height: auto;
+  width: autl;
+  padding-left: 4px;
+  padding-right: 4px;
+  border-radius: 6px;
+  margin: 0;
+  &:hover {
+    background-color: #efe6f2;
+  }
+`
+
+class SearchAn
