6bcdedccfd4db20901279de8dc44c3f15e2b2809
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2021-03-11 15:25:59 +0000

    Update moonlight-common-c

--- client/history.js
+++ client/history.js
@@ -10,4 +10,7 @@ const history =
 
     history.listen((location, action) => {
       ReactGA.set({ page: location.pathname })
-      ReactGA.pageview
+      ReactGA.pageview(location.pathname)
+    })
+
+export default history
