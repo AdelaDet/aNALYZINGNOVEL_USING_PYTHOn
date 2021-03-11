fb39e5e4127a97bf5ae55876ddb6ac090a544457
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2021-03-11 16:36:40 +0000

    build: Updating mediawiki/mediawiki-phan-config to 0.9.0

--- client/routes.js
+++ client/routes.js
@@ -1,1 +1,13 @@
-import React, 
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
+import PropTypes from 'prop-types'
+import {Login, Signup, UserHome, UserDashboard, CategorySinglePage, HomePage, About, Resource} from './components'
+import {PublicSinglePath,DraggableListContainer } from './components/paths'
+import {me} from './store'
+
+
+
+class Routes extends Component {
+  componentDidMount() {
+    this.props.loadIni
