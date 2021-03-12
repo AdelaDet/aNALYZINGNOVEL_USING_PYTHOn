bdbfd2d84258cb9bc9d727acc33ccc83fa5393af
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2021-03-12 10:17:08 +0000

    Version bump to 0.7.6

--- client/store/index.js
+++ client/store/index.js
@@ -1,1 +1,16 @@
-import {createStore, combineReducers, applyMiddleware} from 'red
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import createLogger from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import user from './user'
+import category from './category'
+import {pathReducer} from './pathReducer'
+import step from './step'
+import singleCategory from './single-category'
+import resource from './resource'
+import searchMatches from './search'
+import reviews from './reviews'
+
+const reducer = combineReducers({
+  user,
+  pathRe
