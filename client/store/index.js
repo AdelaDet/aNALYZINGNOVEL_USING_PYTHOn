b86c8ff64310464af2d1dabca1a82019288c677a
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2021-03-12 14:04:10 +0000

    fix indexing

--- client/store/index.js
+++ client/store/index.js
@@ -13,4 +13,24 @@ import reviews from './reviews'
 
 const reducer = combineReducers({
   user,
-  pathRe
+  pathReducer,
+  category,
+  step,
+  singleCategory,
+  resource,
+  searchMatches,
+  reviews
+})
+const middleware = composeWithDevTools(
+  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
+)
+const store = createStore(reducer, middleware)
+
+export default store
+export * from './user'
+export * from './category'
+export * from './pathReducer'
+export * from './step'
+export * from './single-category'
+export * from './search'
+export * from './reviews'
