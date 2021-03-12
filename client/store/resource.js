2f48d73a2a374b85d599caa46043f2d7a882f13e
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2021-03-12 14:45:30 +0000

    New build

--- client/store/resource.js
+++ client/store/resource.js
@@ -5,4 +5,23 @@ const SET_ALL_REVIEWS_OF_RESOURCE = 'SET_ALL_REVIEWS_OF_RESOURCE'
 const setAllReviewsOfResource = (reviews) => {
   return {
     type: SET_ALL_REVIEWS_OF_RESOURCE,
-    
+    reviews,
+  }
+}
+
+export const getAllReviewsOfResource = (resourceName) => {
+  return async (dispatch) => {
+    const res = await axios.get(`/api/resources/${resourceName}/reviews`)
+    dispatch(setAllReviewsOfResource(res.data))
+  }
+}
+
+const initialState = {
+  reviews: {},
+}
+
+export default function( state = initialState, action ){ // eslint-disable-line
+  switch (action.type) {
+    case SET_ALL_REVIEWS_OF_RESOURCE:
+      const combinedReviews = Object.assign(state.reviews, action.reviews)
+      r
