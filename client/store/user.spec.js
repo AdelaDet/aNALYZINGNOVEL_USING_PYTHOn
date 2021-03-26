ebbfeadd81a2e92836d30c0fa3b3ce77c882b559
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2021-03-26 11:17:42 +0000

    Update feed.json

--- client/store/user.spec.js
+++ client/store/user.spec.js
@@ -29,3 +29,16 @@ describe('thunk creators', () => {
 
   describe('me', () => {
     it('eventually dispatches the GET USER action', async () => {
+      const fakeUser = {email: 'Cody'}
+      mockAxios.onGet('/auth/me').replyOnce(200, fakeUser)
+      await store.dispatch(me())
+      const actions = store.getActions()
+      expect(actions[0].type).to.be.equal('GET_USER')
+      expect(actions[0].user).to.be.deep.equal(fakeUser)
+    })
+  })
+
+  describe('logout', () => {
+    it('logout: eventually dispatches the REMOVE_USER action', async () => {
+      mockAxios.onPost('/auth/logout').replyOnce(204)
+      await store.dispatch(logout(
