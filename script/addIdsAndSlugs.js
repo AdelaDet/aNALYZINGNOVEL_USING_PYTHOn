bc0f05db2d2fbb94b4c7b6ea7889286e46f88b10
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-06-20 21:21:50 +0000

    add build status image

---
+++ script/addIdsAndSlugs.js
@@ -0,0 +1,4 @@
+// let neo4j = require('neo4j-driver').v1;
+// let driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "1234"))
+// let session = driver.session();
+let session = require('../server/db/neo
