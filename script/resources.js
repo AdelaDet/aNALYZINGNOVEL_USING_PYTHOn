dbb3787fd89278c1c00fc7cefa167aa47e6ae808
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-06-30 13:44:29 +0000

    More tests added

--- script/resources.js
+++ script/resources.js
@@ -4,4 +4,20 @@
 const resourcesPath1 = [
     {
     url : 'https://www.styled-components.com/docs/basics' ,
-    nam
+    name : 'styled components docs' ,
+    type : 'website'
+    },
+    {
+      url : 'https://medium.com/styled-components/styled-components-getting-started-c9818acbcbbd',
+      name :  'styled components getting started',
+      type : 'article'
+
+    },
+    {
+      url: 'https://medium.freecodecamp.org/a-5-minute-intro-to-styled-components-41f40eb7cd55',
+      name: '5-minute to Styled Components',
+      type: 'article'
+    },
+    {
+      url: 'https://youtu.be/bIK2NwoK9xk',
+      name
