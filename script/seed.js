4a30965e5ae9413a449af105b858d8f97a5fa1fa
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-07-09 21:02:30 +0000

    Added missing comma

--- script/seed.js
+++ script/seed.js
@@ -4,4 +4,16 @@
 // const {User} = require('../server/db/models')
 
 // /**
-//  * We
+//  * Welcome to the seed file! This seed file uses a newer language feature called...
+//  *
+//  *                  -=-= ASYNC...AWAIT -=-=
+//  *
+//  * Async-await is a joy to use! Read more about it in the MDN docs:
+//  *
+//  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+//  *
+//  * Now that you've got the main idea, check it out in practice below!
+//  */
+
+// async function seed() {
+//   await
