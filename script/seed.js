e64a569ed6d9c10b57c7f8fe0560f97390bf6b8b
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-07-09 21:48:20 +0000

    Changed color

--- script/seed.js
+++ script/seed.js
@@ -16,4 +16,13 @@
 //  */
 
 // async function seed() {
-//   await
+//   await db.sync({force: true})
+//   console.log('db synced!')
+//   // Whoa! Because we `await` the promise that db.sync returns, the next line will not be
+//   // executed until that promise resolves!
+//   const users = await Promise.all([
+//     User.create({email: 'cody@email.com', password: '123'}),
+//     User.create({email: 'murphy@email.com', password: '123'})
+//   ])
+//   // Wowzers! We can even `await` on the right-hand side of the assignment operator
+/
