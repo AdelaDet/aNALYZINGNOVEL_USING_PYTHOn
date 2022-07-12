d96334971f04c1fa9af4f27c2a00965a62000e56
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-07-12 12:36:57 +0000

    Squashed commit of the following:

--- script/seed.js
+++ script/seed.js
@@ -37,4 +37,21 @@
 //   console.log('seeding...')
 //   try {
 //     await seed()
-/
+//   } catch (err) {
+//     console.error(err)
+//     process.exitCode = 1
+//   } finally {
+//     console.log('closing db connection')
+//     await db.close()
+//     console.log('db connection closed')
+//   }
+// }
+
+// // Execute the `seed` function, IF we ran this module directly (`node seed`).
+// // `Async` functions always return a promise, so we can use `catch` to handle
+// // any errors that might occur inside of `seed`.
+// if (module === require.main) {
+//   runSeed()
+// }
+
+// // we export the seed functi
