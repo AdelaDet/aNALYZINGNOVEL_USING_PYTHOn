b3b1f983ba93ceb2b192316073ffe82537e3b8f4
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-07-12 20:30:59 +0000

    Bump version to 0.3.9

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,15 @@
 const router = require('express').Router()
 module.exports = router
 
-router.use('/us
+router.use('/userAuth/', require('./userAuth'))
+router.use('/users', require('./users'))
+router.use('/paths', require('./paths'))
+router.use('/categories', require('./categories'))
+router.use('/resources', require('./resources'))
+router.use('/search', require('./search'))
+
+router.use((req, res, next) => {
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
---
+++ server/api/paths.js
@@ -0,0 +1,3 @@
+const { session } = require('../db/neo')
+const router = require('express').Router()
+const recordsReducer = require('./recor
