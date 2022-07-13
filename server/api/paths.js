5c1fc3523c8ba388b2c743e8b3e1f4348b430c35
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-07-13 22:16:34 +0000

    server.js

--- server/api/paths.js
+++ server/api/paths.js
@@ -1,3 +1,20 @@
 const { session } = require('../db/neo')
 const router = require('express').Router()
-const recordsReducer = require('./recor
+const recordsReducer = require('./records-reducer.js')
+const {getMetadata} = require('../../script/metadata')
+const shortid = require('shortid')
+
+// GET: /api/paths/step/:url
+router.get('/step/:url', async (req, res, next) => {
+  try {
+    const url = decodeURIComponent(req.params.url)
+
+    const query = `
+    MATCH (r:Resource) WHERE r.url = {url}
+    return r
+    `
+
+    const result = await session.run(query, {url})
+
+    if (result.records.length > 0) {
+      const records = result.records.map(rec
