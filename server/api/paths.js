a67f7936a7ecf61aea6cd91c66752b24104fbadc
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-07-16 08:36:28 +0000

    Auto update latest refs.

--- server/api/paths.js
+++ server/api/paths.js
@@ -17,4 +17,23 @@ router.get('/step/:url', async (req, res, next) => {
     const result = await session.run(query, {url})
 
     if (result.records.length > 0) {
-      const records = result.records.map(rec
+      const records = result.records.map(record => {
+        return record._fields
+      })
+      res.send(records[0][0].properties)
+    } else {
+      let md = await getMetadata(url)
+      res.send(md)
+    }
+
+    session.close()
+  } catch (err) {
+    next(err)
+  }
+})
+
+// returns the most popular paths (regardless of category)
+// GET: api/paths/popular
+router.get('/popular', async (req,res,next) => {
+  const query = `
+    MATCH (u: User)-[_p:PATHS]->(p: Path {status: 'public'})<-[_r: REVIEWS]-(r:Review),
