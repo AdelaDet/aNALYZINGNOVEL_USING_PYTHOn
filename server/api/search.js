6f134bdfacaec665a62f5baf90138c9ae0ce9fa8
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-09-24 18:13:42 +0000

    Resume updates

--- server/api/search.js
+++ server/api/search.js
@@ -5,4 +5,23 @@ const shortid = require('shortid');
 
 router.post('/', async (req, res, next) => {
   try{
- 
+    const {searchString} = req.body
+    const query = `MATCH (p:Path)
+    WHERE toLower(p.name) CONTAINS toLower({searchString})
+    RETURN p AS matches
+    UNION
+    MATCH (c:Category)
+    WHERE toLower(c.name) CONTAINS toLower({searchString})
+    RETURN c AS matches`
+    const response = await session.run(query, {searchString})
+    const fuzzyMatch = response.records
+    res.json(fuzzyMatch)
+  }catch(err){
+    next(err)
+  }
+})
+
+
+module.exports = router
+
+
