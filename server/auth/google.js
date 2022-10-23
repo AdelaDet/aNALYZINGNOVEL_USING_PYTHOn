d6a2ea74145dcfec6935754d79b37ecea3887651
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-10-23 10:11:44 +0000

    more bug fixing

--- server/auth/google.js
+++ server/auth/google.js
@@ -19,4 +19,21 @@ module.exports = router
  */
 
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
-  console.log('Google client ID / secret not found. Skipping Go
+  console.log('Google client ID / secret not found. Skipping Google OAuth.')
+} else {
+  const googleConfig = {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+  }
+
+  const strategy = new GoogleStrategy(
+    googleConfig,
+    (token, refreshToken, profile, done) => {
+      const googleId = profile.id
+      const name = profile.displayName
+      const email = profile.emails[0].value
+
+      User.findOrCreate({
+        where: {googleId},
+        defaul
