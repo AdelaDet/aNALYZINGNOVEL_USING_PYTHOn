1491a29c708208bbe37625a6f69311ecd43b682d
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-10-23 16:28:29 +0000

    Fix docker build

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,4 +1,18 @@
 const { session, driver } = require('../db/neo')
 const router = require('express').Router()
 const User = require('../db/models/user')
-con
+const crypto = require('crypto')
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    const name = req.body.name
+    const email = req.body.email
+    const salt = crypto.randomBytes(16).toString('base64')
+    const pass = crypto
+      .createHash('RSA-SHA256')
+      .update(req.body.password)
+      .update(salt)
+      .digest('hex')
+
+    const query = `
+    CREATE (newuser:User {name: {name}, email: {email}, password: {password}, googleId: '',
