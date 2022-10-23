90aea87ca623f36d3dd041962c923cffcee94350
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-10-23 08:03:51 +0000

    Version bump to 0.9.5

--- server/auth/google.js
+++ server/auth/google.js
@@ -1,3 +1,12 @@
 const passport = require('passport')
 const router = require('express').Router()
-const GoogleStra
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+const {User} = require('../db/models')
+module.exports = router
+
+/**
+ * For OAuth keys and other secrets, your Node process will search
+ * process.env to find environment variables. On your production server,
+ * you will be able to set these environment variables with the appropriate
+ * values. In development, a good practice is to keep a separate file with
+ * these secrets that you only share with your team - it should NOT b
