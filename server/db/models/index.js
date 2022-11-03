7f48038eb8be6f9e92348637ec2e45751a943c0e
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-11-03 13:25:24 +0000

    glorious funtoo updates

---
+++ server/db/models/index.js
@@ -0,0 +1,11 @@
+const User = require('./user')
+
+/**
+ * If we had any associations to make, this would be a great place to put them!
+ * ex. if we had another model called BlogPost, we might say:
+ *
+ *    BlogPost.belongsTo(User)
+ */
+
+/**
+ * We'll export all of our models here, so that any ti
