d0e680a93cc482d8f2332dbd4a8effc6acb6c316
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-11-03 18:38:46 +0000

    Add a note.

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -8,4 +8,11 @@ const User = require('./user')
  */
 
 /**
- * We'll export all of our models here, so that any ti
+ * We'll export all of our models here, so that any time a module needs a model,
+ * we can just require it from 'db/models'
+ * for example, we can say: const {User} = require('../db/models')
+ * instead of: const User = require('../db/models/user')
+ */
+module.exports = {
+  User
+}
---
+++ server/db/models/user.spec.js
@@ -0,0 +1,8 @@
+// /* global describe beforeEach it */
+
+// const {expect} = require('chai')
+// const db = require('../index')
+// const User = db.model('user')
+
+// describe('User model', () => {
+//   
