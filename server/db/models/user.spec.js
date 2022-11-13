6b3d4a18c22fc1ccb164ab1c8e02a7717d6c1dfc
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-11-13 12:28:36 +0000

    test CI

--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -5,4 +5,23 @@
 // const User = db.model('user')
 
 // describe('User model', () => {
-//   
+//   beforeEach(() => {
+//     return db.sync({force: true})
+//   })
+
+//   describe('instanceMethods', () => {
+//     describe('correctPassword', () => {
+//       let cody
+
+//       beforeEach(async () => {
+//         cody = await User.create({
+//           email: 'cody@puppybook.com',
+//           password: 'bones'
+//         })
+//       })
+
+//       it('returns true if the password is correct', () => {
+//         expect(cody.correctPassword('bones')).to.be.equal(true)
+//       })
+
+//     
