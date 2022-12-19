2277cc508b99b446940ace0a5ed661f753cb6be7
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-12-19 09:20:32 +0000

    Translated using Weblate (French (France))

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const compression = require('compression')
+const sessionExpress = require('express-session')
+con
