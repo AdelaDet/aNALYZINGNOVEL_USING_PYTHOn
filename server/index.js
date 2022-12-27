7c8a8c568f051f548b6973ee8f92d59464a9d180
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2022-12-27 21:57:41 +0000

    Docs improvements

--- server/index.js
+++ server/index.js
@@ -113,4 +113,18 @@ const startListening = () => {
 
   // set up our socket control center
   const io = socketio(server)
-  require('./s
+  require('./socket')(io)
+}
+
+// const syncDb = () => db.sync()
+
+async function bootApp() {
+  // await sessionStore.sync()
+  // await syncDb()
+  await createApp()
+  await startListening()
+}
+// This evaluates as true when this file is run directly from the command line,
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this module is required by another module - for example,
+// if we wanted to require our app
