b508185f0265634848ee46e0a4cfdc60d185b333
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2023-02-22 06:21:25 +0000

    Minor typo fixes

--- tests/server/api/paths.ignore.js
+++ tests/server/api/paths.ignore.js
@@ -7,3 +7,19 @@ const expect = chai.expect
 let neo4j = require('neo4j-driver').v1
 let driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '1234'))
 let session = driver.session()
+const app = require('../../../server/index.js')
+const agent = require('supertest')(app)
+
+xdescribe('Paths API Routes', () => {
+  before(async () => {
+    //re-seed db?
+  })
+
+  xdescribe('GET /api/paths/all/user/:username/', () => {
+    it('returns all of a users paths', async () => {
+      const response = await agent.get('/api/paths/all/user/Jami').expect(200)
+    })
+  })
+
+  xdescribe('/api/paths/step/:url', () => {
+    it('returns the resource given in the URL', async 
