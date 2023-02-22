575a4af9a644eb42c651649fd8b258636a8a705d
AdelaDet <96376169+AdelaDet@users.noreply.github.com>
2023-02-22 17:31:34 +0000

    USB: serial: Fix memory leak in sierra_release()

--- tests/server/api/userAuth/paths.spec.js
+++ tests/server/api/userAuth/paths.spec.js
@@ -1,2 +1,22 @@
 const { expect } = require('chai')
-const request = require('supertest
+const request = require('supertest')
+const app = require('../../../../server/index.js')
+let { session, driver } = require('../../../../server/db/neo')
+const crypto = require('crypto')
+
+const user = {
+  name: 'testUser',
+  password: '1234',
+  email: 'test@user.com'
+}
+
+const createTestUser = async () => {
+  const salt = crypto.randomBytes(16).toString('base64')
+  const password = crypto
+    .createHash('RSA-SHA256')
+    .update('1234')
+    .update(salt)
+    .digest('hex')
+
+  const query = `
+  CREATE (newuser:Us
